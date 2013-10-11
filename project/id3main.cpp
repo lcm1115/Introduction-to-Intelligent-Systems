a6f184ee7758de6ef7fa8714b2e18f8ebcd88168
Liam Morris <lcm1115@rit.edu>
2013-10-11 15:28:09 -0400

    Removed extra #incudes

--- project/id3main.cpp
+++ project/id3main.cpp
@@ -1,29 +1,15 @@
 #include "constructtree.h"
 #include "id3.h"
 
-#include <cstring>
 #include <iostream>
-#include <fstream>
-#include <map>
-#include <sstream>
 
 using id3::construct_tree;
-using id3::entropy;
-using id3::info_gain;
 using id3::node;
 using id3::tree_to_string;
 using id3::read_nodes_from_filepath;
-using id3::split_nodes;
-using id3::subset_entropy;
 using id3::vertex;
-using std::cin;
 using std::cout;
-using std::getline;
-using std::endl;
-using std::ifstream;
-using std::map;
 using std::string;
-using std::stringstream;
 using std::vector;
 
 int main(int argc, char** argv) {
