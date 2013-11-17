43f9a46a4d178d227c3f2519d6b1ae5a007e3636
Liam Morris <lcm1115@rit.edu>
2013-11-17 14:33:58 -0500

    Added comments to c45 files.

--- project/c45.cpp
+++ project/c45.cpp
@@ -1,7 +1,6 @@
 #include "c45.h"
 #include "decision.h"
 
-#include <algorithm>
 #include <cmath>
 #include <cstdio>
 #include <fstream>
@@ -13,127 +12,127 @@ using decision::lg;
 using decision::string_to_tokens;
 using std::ifstream;
 using std::map;
-using std::sort;
 using std::string;
 using std::vector;
 
 namespace c45 {
-    vector<node> read_nodes_from_filepath(const string& filepath) {
-        vector<node> nodes;
-        vector<string> values;
-        map<string, char> value_types;
-        vector<string> tokens;
-        ifstream input_file(filepath.c_str());
-        string line;
 
-        // Get value types.
-        getline(input_file, line);
-        tokens = string_to_tokens(line, ' ');
-        for (int i = 0; i < tokens.size(); ++i) {
-            int split_index = tokens.at(i).find(':');
-            string value_name = tokens.at(i).substr(0, split_index);
-            char value_type = tokens.at(i).at(split_index + 1);
-            value_types[value_name] = value_type;
-            values.push_back(value_name);
-        }
+vector<node> read_nodes_from_filepath(const string& filepath) {
+    vector<node> nodes;
+    vector<string> values;
+    map<string, char> value_types;
+    vector<string> tokens;
+    ifstream input_file(filepath.c_str());
+    string line;
 
-        while (getline(input_file, line)) {
-            node new_node;
+    // Get value types.
+    getline(input_file, line);
+    tokens = string_to_tokens(line, ' ');
 
-            vector<string> tmp_values = string_to_tokens(line, ' ');
-
-            for (int i = 0; i < tmp_values.size(); ++i) {
-                if (value_types[values.at(i)] == 'D') {
-                    new_node.string_values[values.at(i)] = tmp_values.at(i);
-                } else {
-                    new_node.cont_values[values.at(i)] =
-                        atof(tmp_values.at(i).c_str());
-                }
-            }
-
-            nodes.push_back(new_node);
-        }
-
-        input_file.close();
-        return nodes;
+    // Determine which values are continuous and which are discrete.
+    for (int i = 0; i < tokens.size(); ++i) {
+        int split_index = tokens.at(i).find(':');
+        string value_name = tokens.at(i).substr(0, split_index);
+        char value_type = tokens.at(i).at(split_index + 1);
+        value_types[value_name] = value_type;
+        values.push_back(value_name);
     }
-    void partition_sizes(int count[],
-                         const vector<node>& data,
-                         const string& attr,
-                         double partition) {
-        count[0] = 0;
-        count[1] = 0;
-        for (vector<node>::const_iterator it = data.begin();
-             it != data.end(); ++it) {
-            if (it->cont_values.at(attr) <= partition) {
-                ++count[0];
+
+    while (getline(input_file, line)) {
+        node new_node;
+
+        vector<string> tmp_values = string_to_tokens(line, ' ');
+
+        for (int i = 0; i < tmp_values.size(); ++i) {
+            // Add value to corresponding value list depending on its type.
+            if (value_types[values.at(i)] == 'D') {
+                new_node.string_values[values.at(i)] = tmp_values.at(i);
             } else {
-                ++count[1];
-            }
-        }
-    }
-
-    double cont_entropy(
-            const vector<node>& data, const string& attr, double partition) {
-        int count[] = { 0, 0 };
-        partition_sizes(count, data, attr, partition);
-        double set_entropy = 0.0;
-
-        double p;
-        if (count[0] > 0) {
-            p = (double) count[0] / data.size();
-            set_entropy += -p * lg(p);
-        }
-        if (count[1] > 0) {
-            p = (double) count[1] / data.size();
-            set_entropy += -p * lg(p);
-        }
-
-        return set_entropy;
-    }
-
-    map<string, int> count_occurrences(
-            const vector<node>& data, const string& key) {
-        map<string, int> value_count;
-       
-        // Iterate across each value and count how many times it appears.
-        for (vector<node>::const_iterator it = data.begin();
-             it != data.end(); ++it) {
-            if (value_count.find(
-                        it->string_values.at(key)) != value_count.end()) {
-                ++value_count[it->string_values.at(key)];
-            } else {
-                value_count[it->string_values.at(key)] = 1;
+                new_node.cont_values[values.at(i)] =
+                    atof(tmp_values.at(i).c_str());
             }
         }
 
-        return value_count;
+        nodes.push_back(new_node);
     }
 
-    double ideal_partition(const vector<node>& data,
-                           const string& attr) {
-        vector<double> values(data.size());
-        for (vector<node>::const_iterator it = data.begin();
-             it != data.end(); ++it) {
-            values.push_back(it->cont_values.at(attr));
-        }
-        double max_ent = 0;
-        double partition = values.at(0);
-
-        for (int i = 0; i < values.size() - 1; ++i) {
-            double ent = cont_entropy(data, attr, values.at(i));
-            if (ent > max_ent) {
-                max_ent = ent;
-                partition = values.at(i);
-            }
-        }
-
-        return partition;
-    }
-}  // namespace c45
-
-using namespace c45;
-
-int main(int argc, char** argv) {
-    vector<node> nodes = read_nodes_from_filepath(argv[1]);
+    input_file.close();
+    return nodes;
 }
+
+void partition_sizes(int count[],
+                     const vector<node>& data,
+                     const string& attr,
+                     double partition) {
+    count[0] = 0;
+    count[1] = 0;
+    // Count size of each partition.
+    for (vector<node>::const_iterator it = data.begin();
+         it != data.end(); ++it) {
+        if (it->cont_values.at(attr) <= partition) {
+            ++count[0];
+        } else {
+            ++count[1];
+        }
+    }
+}
+
+double cont_entropy(
+        const vector<node>& data, const string& attr, double partition) {
+    int count[] = { 0, 0 };
+    partition_sizes(count, data, attr, partition);
+    double set_entropy = 0.0;
+
+    double p;
+    if (count[0] > 0) {
+        p = (double) count[0] / data.size();
+        set_entropy += -p * lg(p);
+    }
+    if (count[1] > 0) {
+        p = (double) count[1] / data.size();
+        set_entropy += -p * lg(p);
+    }
+
+    return set_entropy;
+}
+
+map<string, int> count_occurrences(
+        const vector<node>& data, const string& key) {
+    map<string, int> value_count;
+   
+    // Iterate across each value and count how many times it appears.
+    for (vector<node>::const_iterator it = data.begin();
+         it != data.end(); ++it) {
+        if (value_count.find(
+                    it->string_values.at(key)) != value_count.end()) {
+            ++value_count[it->string_values.at(key)];
+        } else {
+            value_count[it->string_values.at(key)] = 1;
+        }
+    }
+
+    return value_count;
+}
+
+double ideal_partition(const vector<node>& data,
+                       const string& attr) {
+    vector<double> values(data.size());
+    for (vector<node>::const_iterator it = data.begin();
+         it != data.end(); ++it) {
+        values.push_back(it->cont_values.at(attr));
+    }
+    double max_ent = 0;
+    double partition = values.at(0);
+
+    for (int i = 0; i < values.size() - 1; ++i) {
+        double ent = cont_entropy(data, attr, values.at(i));
+        if (ent > max_ent) {
+            max_ent = ent;
+            partition = values.at(i);
+        }
+    }
+
+    return partition;
+}
+
+}  // namespace c45
--- project/c45.h
+++ project/c45.h
@@ -3,17 +3,42 @@
 #include <vector>
 
 namespace c45 {
-    struct node {
-        std::map<std::string, std::string> string_values;
-        std::map<std::string, double> cont_values;
-    };
 
-    std::vector<node> read_nodes_from_filepath(const std::string& filepath);
+struct node {
+    std::map<std::string, std::string> string_values;
+    std::map<std::string, double> cont_values;
+};
 
-    double cont_entropy(const std::vector<node>& data,
-                        const std::string& attr,
-                        double partition);
+// Given a filepath, reads all lines from the file and creates a node from each.
+// 'filepath' the filepath to the data file that contains node information
+// Returns a vector that contains all nodes created from the data file.
+std::vector<node> read_nodes_from_filepath(const std::string& filepath);
 
-    double ideal_partition(
-            const std::vector<node>& data, const std::string& attr);
+// Counts the sizes of each partition of a set of nodes.
+// 'count' is the array in which the sizes will be stored
+// 'data' is the list of nodes
+// 'attr' is the continuous attribute on which partitioning occurs
+// 'partition' is the threshold for splitting the set
+void partition_sizes(int count[],
+                     const std::vector<node>& data,
+                     const std::string& attr,
+                     double partition);
+
+// Determines the entropy of a set when partitioned by a value.
+// 'data' is the list of nodes
+// 'attr' is the continuous attribute
+// 'partition' is the partition threshold for the splitting
+// Returns a double that represents the entropy of the set.
+double cont_entropy(const std::vector<node>& data,
+                    const std::string& attr,
+                    double partition);
+
+// Determines the ideal partition for a set for a continuous attribute.
+// 'data' is the list of nodes
+// 'attr' is the continuous attribute for which a partition is being found
+// Returns a double representing the value on which the set should be
+// partitioned.
+double ideal_partition(
+        const std::vector<node>& data, const std::string& attr);
+
 }  // namespace c45
